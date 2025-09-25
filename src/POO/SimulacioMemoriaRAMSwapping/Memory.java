e3efdbfed2776a257f73bc5f856cd0298710e16a
bluePixie77 <mariaramisbarrera@gmail.com>
2025-09-25 10:38:06 +0200

    Canvis

--- src/POO/SimulacioMemoriaRAMSwapping/Memory.java
+++ src/POO/SimulacioMemoriaRAMSwapping/Memory.java
@@ -1,5 +1,7 @@
 package POO.SimulacioMemoriaRAMSwapping;
 
+import processing.core.PApplet;
+
 public class Memory {
 
     // Atributs o propietats
@@ -18,4 +20,53 @@ public class Memory {
 
     // Altres mètodes
 
+    void display(PApplet p5){
+
+        p5.stroke(0); p5.strokeWeight(3); p5.fill(220);
+        p5.rect(100, 100, mida, 300);
+
+        p5.fill(0); p5.textSize(20); p5.textAlign(p5.LEFT);
+        p5.text("Memòria RAM", 100, 60);
+        p5.text("Total: "+this.mida, 100, 400);
+        //p5.text("Memoria ocupada: ", x);
+
+
+        for(int i=0; i<procesos.length; i++){
+            if(procesos[i] != null) {
+                procesos[i].display(p5, 100, 100, 300);
+            }
+        }
+    }
+
+    float calcularMemoriaOcupada(){
+        float memoriaOcupada = 0;
+        for(int i=0; i<procesos.length; i++){
+            if(procesos[i] != null){
+                memoriaOcupada += procesos[i].mida;
+            }
+        }
+        return memoriaOcupada;
+    }
+    void swapIn(Proces p){
+        if(freeAddress+p.mida < mida){
+            procesos[numProcesos] = p;
+            numProcesos++;
+            p.setAddress(freeAddress);
+            freeAddress += p.mida;
+        }else{
+            System.out.println("ERROR de SWAP IN del procés "+p.nom+".");
+        }
+    }
+
+    void swapOut(Proces p){
+        for(int i=0; i<numProcesos; i++){
+            if(procesos[i] == p){
+                procesos[i] = null;
+                numProcesos--;
+                System.out.println("SWAPPED OUT "+p.nom);
+
+            }
+
+        }
+    }
 }
